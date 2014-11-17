CyanogenMod/android_hardware_libhardware

libhardware: Add ifdef around allocSize
 * Non-QC devices with prebuilt grallocs hate this.

Change-Id: I57ce7547e99ec0cf40c4f05f3742348dec4e2d9d

include/hardware/gralloc.h
@@ -278,6 +278,7 @@ typedef struct gralloc_module_t {
 typedef struct alloc_device_t {
     struct hw_device_t common;
 
+#ifdef QCOM_HARDWARE
     /*
      * (*allocSize)() Allocates a buffer in graphic memory with the requested
      * bufferSize parameter and returns a buffer_handle_t and the stride in
@@ -290,6 +291,7 @@ typedef struct alloc_device_t {
     int (*allocSize)(struct alloc_device_t* dev,
             int w, int h, int format, int usage,
             buffer_handle_t* handle, int* stride, int bufferSize);
+#endif
 
     /* 
      * (*alloc)() Allocates a buffer in graphic memory with the requested

SlimRoms/hardware_libhardware

include/hardware/gralloc.h
@@ -307,6 +307,7 @@ typedef struct gralloc_module_t {
 typedef struct alloc_device_t {
     struct hw_device_t common;
 
+#ifdef QCOM_BSP
     /*
      * (*allocSize)() Allocates a buffer in graphic memory with the requested
      * bufferSize parameter and returns a buffer_handle_t and the stride in
@@ -319,6 +320,7 @@ typedef struct alloc_device_t {
     int (*allocSize)(struct alloc_device_t* dev,
             int w, int h, int format, int usage,
             buffer_handle_t* handle, int* stride, int bufferSize);
+#endif
 
     /* 
      * (*alloc)() Allocates a buffer in graphic memory with the requested